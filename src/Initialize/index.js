a4b02daba1368aa7aaee9dbccf4afa0170412379
pmpurcell93 <maddenpurcell@gmail.com>
2021-10-05 21:46:35 -0500

    Built joke generator alongside class

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -1,37 +1,50 @@
 import React, { useState } from 'react';
+import getJoke from '../getJoke';
 
 function Initialize() {
-  const [domWriting, setDomWriting] = useState('Nothing Here!');
+  const [btnText, setBtnText] = useState('Get a Joke');
+  const [joke, setJoke] = useState({});
+  console.warn(joke);
+  console.warn(setBtnText);
 
-  const handleClick = (e) => {
-    console.warn(`You clicked ${e.target.id}`);
-    setDomWriting(`You clicked ${e.target.id}! Check the Console!`);
+  const setButton = (text) => {
+    setBtnText(text);
+  };
+
+  const getAJoke = () => {
+    getJoke().then((obj) => {
+      setJoke({
+        setup: obj.setup,
+        punchline: obj.delivery,
+      });
+      setButton('Get Punchline');
+    });
   };
 
   return (
     <div className="App">
-      <h2>INSIDE APP COMPONENT</h2>
+      <h2>{joke.setup}</h2>
+      <h5>{btnText !== 'Get Punchline' ? joke.punchline : ''}</h5>
       <div>
-        <button
-          type="button"
-          id="this-button"
-          className="btn btn-info"
-          onClick={handleClick}
-        >
-          I am THIS button
-        </button>
+        {btnText === 'Get a Joke' || btnText === 'Get Another Joke' ? (
+          <button
+            onClick={getAJoke}
+            type="button"
+            className="btn btn-primary mt-3"
+          >
+            {btnText}
+          </button>
+        ) : (
+          <button
+            type="button"
+            id="joke-button"
+            className="btn btn-primary mt-3"
+            onClick={() => setButton('Get Another Joke')}
+          >
+            {btnText}
+          </button>
+        )}
       </div>
-      <div>
-        <button
-          type="button"
-          id="that-button"
-          className="btn btn-primary mt-3"
-          onClick={handleClick}
-        >
-          I am THAT button
-        </button>
-      </div>
-      <h3>{domWriting}</h3>
     </div>
   );
 }
